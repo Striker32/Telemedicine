234d65faf825e26f213a7d48e6014222eb504e30
Paladin <ang-laro@yandex.ru>
2025-10-27 01:25:01 +0300

    Предпоследний коммит перед пиздецом

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:last_telemedicine/pages/Choose_profile.dart';
-import 'package:last_telemedicine/pages/user_pages/login_user.dart';
-import 'package:last_telemedicine/pages/user_pages/profile_user.dart';
-import 'package:last_telemedicine/pages/user_pages/register_user.dart';
-import 'package:last_telemedicine/pages/user_pages/applications_user.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:last_telemedicine/auth/auth_gate.dart';
 import 'package:last_telemedicine/auth/login_or_register.dart';
+import 'package:last_telemedicine/themes/main_mode.dart';
 import 'package:last_telemedicine/firebase_options.dart';
 import 'package:last_telemedicine/pages/login_page.dart';
-import 'package:last_telemedicine/themes/main_mode.dart';
+
+// Страницы юзера
+import 'package:last_telemedicine/pages/user_pages/login_user.dart';
+import 'package:last_telemedicine/pages/user_pages/profile_user.dart';
+import 'package:last_telemedicine/pages/user_pages/register_user.dart';
+import 'package:last_telemedicine/pages/user_pages/applications_user.dart';
+
+// Страницы доктора
+import 'package:last_telemedicine/pages/doctor_pages/main_doctor.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,7 +30,7 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ApplicationsPage(),
+      home: MainDoctor(),
       theme: ThemeData(
         fontFamily: "SF Pro Display",
       ),
