0d9912ce30cd9bbdcef9d2aed9a6c2615f873d06
paladin <ang-laro@yandex.ru>
2025-11-10 16:24:32 +0300

    3

--- lib/components/SplashScreen.dart
+++ lib/components/SplashScreen.dart
@@ -9,6 +9,7 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
+
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   late final AnimationController _controller;
